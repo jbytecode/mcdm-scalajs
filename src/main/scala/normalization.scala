dfd6a1dc80029bdcf1ee7c55ade95cbeccb61300
mhsatman@gmail.com <mhsatman@gmail.com>
2025-03-12 21:48:49 +0300

    add Divide by Column Sums Normalization (default for Aras)

--- src/main/scala/normalization.scala
+++ src/main/scala/normalization.scala
@@ -13,3 +13,12 @@ object Normalization:
     val (n, m) = Matrix.size(decmat)
     val columnnorms = Matrix.applyFunctionToColumns(decmat, Matrix.norm)
     Array.tabulate(n, m)((i, j) => decmat(i)(j) / columnnorms(j))
+
+  def DivideByColumnnsSumNormalization(
+      decmat: Mat,
+      weights: Vec,
+      directions: Array[Direction]
+  ): Mat =
+    val (n, m) = Matrix.size(decmat)
+    val columnsums = Matrix.colsums(decmat)
+    Array.tabulate(n, m)((i, j) => decmat(i)(j) / columnsums(j))
--- src/test/scala/testnormalization.scala
+++ src/test/scala/testnormalization.scala
@@ -2,6 +2,8 @@ import munit.Assertions as A
 
 import scala.math as math
 import org.expr.mcdm.Matrix
+import org.expr.mcdm.Direction._
+import org.expr.mcdm.Normalization._
 
 class TestNormalizations extends munit.FunSuite {
 
@@ -16,6 +18,27 @@ class TestNormalizations extends munit.FunSuite {
       math.sqrt(3.0 * 3.0 + 6.0 * 6.0 + 9.0 * 9.0)
     )
     A.assert(Matrix.elementwise_equal(columnnorms, expected))
+  }
+  test("Divide By Columns Sum Normalization"){
+    val decmat = Array(
+            Array(0.616812,  0.415094 ,  0.0168757,  0.338965 ,  0.190954),
+            Array(0.938958,  0.363936 ,  0.528116 ,  0.739575 ,  0.669537),
+            Array(0.816357,  0.538917 ,  0.306811 ,  0.356321 ,  0.380972),
+            Array(0.874629,  0.0295879,  0.340564 ,  0.0345955,  0.291483))
+
+    val weights = Array(0.2, 0.2, 0.2, 0.2, 0.2)
+
+    val directions = Array(Maximize, Maximize, Minimize, Minimize, Maximize)
+
+    val normalizedMatrix = DivideByColumnnsSumNormalization(decmat, weights, directions)
+
+    val expected = Array(
+       Array(0.189978,  0.30804 ,  0.0141531,  0.230674 ,  0.124567),
+       Array(0.289199,  0.270075,  0.442914 ,  0.503298 ,  0.436765),
+       Array(0.251438,  0.399928,  0.257313 ,  0.242485 ,  0.248523),
+       Array(0.269385,  0.021957,  0.28562  ,  0.0235431,  0.190145))
+       
+    A.assert(Matrix.elementwise_equal(normalizedMatrix, expected, 1e-5))
   }
   
 }
