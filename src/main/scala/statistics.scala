24ebba6bb2aa595f37696ed4a55075542b0bf1fe
mhsatman@gmail.com <mhsatman@gmail.com>
2025-03-17 19:56:46 +0300

    add matrix algebra for estimating linear regression coefficients

--- src/main/scala/statistics.scala
+++ src/main/scala/statistics.scala
@@ -2,31 +2,41 @@ package org.expr.mcdm
 
 object Statistics:
 
-    def mean(a: Vec): Double = a.sum / a.length
+  def mean(a: Vec): Double = a.sum / a.length
 
-    def variance(a: Vec) : Double =
-      val m = mean(a)
-      a.map(x => (x - m) * (x - m)).sum / (a.length - 1.0)
+  def variance(a: Vec): Double =
+    val m = mean(a)
+    a.map(x => (x - m) * (x - m)).sum / (a.length - 1.0)
 
-    def std(a: Vec): Double = math.sqrt(variance(a))
-    
-    def correlation(a: Vec, b: Vec): Double =
-      val ma = mean(a)
-      val mb = mean(b)
-      var va = variance(a)
-      var vb = variance(b)
-      (a.zip(b).map((x, y) => (x - ma) * (y - mb)).sum / math.sqrt(va * vb)) / (a.length - 1.0)
+  def std(a: Vec): Double = math.sqrt(variance(a))
 
-    def correlation(a: Mat): Mat = 
-      val n = a.length
-      val m = a(0).length
-      val cor = Matrix.zeros(m, m)
-      for i <- 0 until m do
-        for j <- 0 until m do
-          cor(i)(j) = correlation(Matrix.getcolat(a, i), Matrix.getcolat(a, j))
-      cor  
+  def correlation(a: Vec, b: Vec): Double =
+    val ma = mean(a)
+    val mb = mean(b)
+    var va = variance(a)
+    var vb = variance(b)
+    (a.zip(b).map((x, y) => (x - ma) * (y - mb)).sum / math.sqrt(
+      va * vb
+    )) / (a.length - 1.0)
 
-    def euclideanDistance(a: Vec, b: Vec): Double =
-        math.sqrt(a.zip(b).map((x, y) => (x - y) * (x - y)).sum)
+  def correlation(a: Mat): Mat =
+    val n = a.length
+    val m = a(0).length
+    val cor = Matrix.zeros(m, m)
+    for i <- 0 until m do
+      for j <- 0 until m do
+        cor(i)(j) = correlation(Matrix.getcolat(a, i), Matrix.getcolat(a, j))
+    cor
 
-    def geomean(x: Vec): Double = math.exp(x.map(math.log).sum / x.length)
+  def euclideanDistance(a: Vec, b: Vec): Double =
+    math.sqrt(a.zip(b).map((x, y) => (x - y) * (x - y)).sum)
+
+  def geomean(x: Vec): Double = math.exp(x.map(math.log).sum / x.length)
+
+  def linearregression(x: Mat, y: Vec): Vec =
+    val n = x.length
+    val m = x(0).length
+    val xtx = Matrix.mul(x.transpose, x)
+    val xty = Matrix.mul(x.transpose, Matrix.makeRowMatrix(y))
+    val beta = Matrix.mul(Matrix.inverse(xtx), xty)
+    beta(0)
--- src/test/scala/teststatistics.scala
+++ src/test/scala/teststatistics.scala
@@ -6,58 +6,69 @@ import org.expr.mcdm.Statistics
 
 class TestStatistics extends munit.FunSuite {
 
-  test("Mean of vector"){
+  test("Mean of vector") {
     val a = Array(1.0, 2.0, 3.0, 4.0, 5.0)
     val mean = Statistics.mean(a)
     val expected = 3.0
     A.assertEquals(mean, expected)
   }
-  test("Variance of vector"){
+  test("Variance of vector") {
     val a = Array(1.0, 2.0, 3.0, 4.0, 5.0)
     val variance = Statistics.variance(a)
     val expected = 2.5
     A.assertEquals(variance, expected)
   }
-  test("Standard deviation of vector"){
+  test("Standard deviation of vector") {
     val a = Array(1.0, 2.0, 3.0, 4.0, 5.0)
     val std = Statistics.std(a)
     val expected = math.sqrt(2.5)
     A.assertEquals(std, expected)
   }
-  test("Correlation of vector with itself"){
+  test("Correlation of vector with itself") {
     val a = Array(1.0, 2.0, 3.0, 4.0, 5.0)
     val covariance = Statistics.correlation(a, a)
     val expected = 1.0
     A.assertEquals(covariance, expected)
   }
-  test("Correlation of two different vectors (-1)"){
+  test("Correlation of two different vectors (-1)") {
     val a = Array(1.0, 2.0, 3.0, 4.0, 5.0)
     val b = Array(5.0, 4.0, 3.0, 2.0, 1.0)
     val covariance = Statistics.correlation(a, b)
     val expected = -1.0
     A.assertEquals(covariance, expected)
   }
-  test("Correlation matrix of columns of matrix"){
-    val a = Array(Array(1.0, 2.0, 3.0), Array(4.0, 5.0, 6.0), Array(7.0, 8.0, 90.0))
+  test("Correlation matrix of columns of matrix") {
+    val a =
+      Array(Array(1.0, 2.0, 3.0), Array(4.0, 5.0, 6.0), Array(7.0, 8.0, 90.0))
     val corr = Statistics.correlation(a)
     val expected = Array(
       Array(1.0, 1.0, 0.880812),
       Array(1.0, 1.0, 0.880812),
-      Array(0.880812, 0.880812, 1.0))
+      Array(0.880812, 0.880812, 1.0)
+    )
     A.assert(Matrix.elementwise_equal(corr, expected, 1e-6))
   }
-  test("Geometric mean (with logarithmic formula)"){
+  test("Geometric mean (with logarithmic formula)") {
 
-     val x = Array(
-       0.9995440160681826,
-       0.7608611955213163,
-       0.8967084105498316,
-       0.9955675288968396,
-       0.007713272360632573)
-     
-     val expected = 0.3497950008915474
+    val x = Array(0.9995440160681826, 0.7608611955213163, 0.8967084105498316,
+      0.9955675288968396, 0.007713272360632573)
 
-     A.assertEqualsDouble(Statistics.geomean(x), expected, 1e-6)
+    val expected = 0.3497950008915474
+
+    A.assertEqualsDouble(Statistics.geomean(x), expected, 1e-6)
   }
-  
+  test("Linear Regression") {
+    val X = Array(
+      Array(1.0, 1.0),
+      Array(1.0, 2.0),
+      Array(1.0, 3.0),
+      Array(1.0, 4.0),
+      Array(1.0, 5.0)
+    )
+    val y = Array(3.0, 5.0, 7.0, 9.0, 11.0)
+    val beta = Statistics.linearregression(X, y)
+    val expected = Array(1.0, 2.0)
+    A.assert(Matrix.elementwise_equal(beta, expected, 1e-6))
+  }
+
 }
