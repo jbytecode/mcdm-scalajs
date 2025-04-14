9b07ceb19bc35ecfc4d8fe15a98b23d3fc1823d7
mhsatman@gmail.com <mhsatman@gmail.com>
2025-04-14 22:19:59 +0300

    merec in more functional style

--- src/main/scala/merec.scala
+++ src/main/scala/merec.scala
@@ -20,9 +20,6 @@ def merec(
   val NormalizeMatrix =
     normalization(decmat, Array.emptyDoubleArray, directions)
 
-  // val S = Matrix.zeros(row)
-  // val S_ = Matrix.zeros(row, col)
-
   val S = Array
     .range(0, row)
     .map(i =>
@@ -49,9 +46,15 @@ def merec(
     )
   )
 
-  val E = Matrix.zeros(col)
-  for j <- 0 until col do
-    E(j) = E(j) + abs(Matrix.subtract(Matrix.getcolat(S_, j), S).sum)
+  val E = Array
+    .range(0, col)
+    .map(j =>
+      abs(
+        Matrix
+          .subtract(Matrix.getcolat(S_, j), S)
+          .sum
+      )
+    )
 
   val w = E.map(_ / E.sum)
 
