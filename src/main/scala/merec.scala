80a8fe651da6c3196190b700ddbad483a0629e73
mhsatman@gmail.com <mhsatman@gmail.com>
2025-04-14 22:18:40 +0300

    merec in more functional style

--- src/main/scala/merec.scala
+++ src/main/scala/merec.scala
@@ -12,49 +12,49 @@ def merec(
     directions: Array[Direction],
     normalization: NormalizationFunction =
       Normalization.InverseDivideByColumnMaxMinNormalization,
-    options: Map[String, Any] = Map.empty 
-): MerecResult = 
+    options: Map[String, Any] = Map.empty
+): MerecResult =
 
-    val (row, col) = Matrix.size(decmat)
+  val (row, col) = Matrix.size(decmat)
 
-    val NormalizeMatrix = normalization(decmat, Array.emptyDoubleArray, directions)
+  val NormalizeMatrix =
+    normalization(decmat, Array.emptyDoubleArray, directions)
 
-    val S = Matrix.zeros(row)
-    val S_ = Matrix.zeros(row, col)
+  // val S = Matrix.zeros(row)
+  // val S_ = Matrix.zeros(row, col)
 
-
-    for i <- 0 until row do
-        S(i) = log(
-            1 + (
-                Matrix.getrowat(NormalizeMatrix, i)
-                    .map(log)
-                    .map(abs)
-                    .sum / col
-            )
+  val S = Array
+    .range(0, row)
+    .map(i =>
+      log(
+        1 + (
+          Matrix
+            .getrowat(NormalizeMatrix, i)
+            .map(log)
+            .map(abs)
+            .sum / col
         )
-    
-
-    for i <- 0 until row do
-        for j <- 0 until col do
-            S_(i)(j) = log(
-                1 + (
-                    (Matrix.getrowat(NormalizeMatrix, i)
-                        .map(log)
-                        .map(abs)
-                        .sum - abs(log(NormalizeMatrix(i)(j)))) / col
-                )
-            )
-
-
-    val E = Matrix.zeros(col)
-    for j <- 0 until col do
-        E(j) = E(j) + abs(Matrix.subtract(Matrix.getcolat(S_, j), S).sum)
-
-
-
-    val w = E.map(_ / E.sum)
-
-
-    MerecResult(
-        weights = w
+      )
     )
+
+  val S_ = Array.tabulate(row, col)((i, j) =>
+    log(
+      1 + (
+        (Matrix
+          .getrowat(NormalizeMatrix, i)
+          .map(log)
+          .map(abs)
+          .sum - abs(log(NormalizeMatrix(i)(j)))) / col
+      )
+    )
+  )
+
+  val E = Matrix.zeros(col)
+  for j <- 0 until col do
+    E(j) = E(j) + abs(Matrix.subtract(Matrix.getcolat(S_, j), S).sum)
+
+  val w = E.map(_ / E.sum)
+
+  MerecResult(
+    weights = w
+  )
